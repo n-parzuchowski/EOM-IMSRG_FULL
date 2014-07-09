3c1070f1f57e604e95b7096be380550f4472f994
Nathan Parzuchowski <parzuchowski@frib.msu.edu>
2014-07-09 12:11:51 -0400

    added a v_elem routine and a H0 calculator for the HO basis

--- basic_IMSRG.f90
+++ basic_IMSRG.f90
@@ -35,7 +35,7 @@ module basic_IMSRG
   TYPE :: sq_op !second quantized operator 
      type(sq_block),allocatable,dimension(:) :: mat
      real(8),allocatable,dimension(:,:) :: fph,fpp,fhh
-     integer :: nblock,nbody,msp,herm
+     integer :: nblock,Abody,Nsp,herm
      real(8) :: E0 
   END TYPE sq_op
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
@@ -132,20 +132,20 @@ subroutine allocate_blocks(jbas,op)
   
   type(spd) :: jbas
   type(sq_op) :: op
-  integer :: n,m,q,i,j,j1,j2,tz1,tz2,l1,l2
+  integer :: N,A,q,i,j,j1,j2,tz1,tz2,l1,l2
   integer :: Jtot,Tz,Par,nph,npp,nhh,CX
   
  
-  n = op%nbody !number of holes
-  op%msp = jbas%total_orbits
-  m = op%msp  !number of sp states
+  A = op%Abody !number of holes
+  op%Nsp = jbas%total_orbits
+  N = op%Nsp  !number of sp states
   
   op%nblock =  (jbas%Jtotal_max + 1) * 6    ! 6 possible values of par * Tz    
   
   allocate(op%mat(op%nblock)) 
-  allocate(op%fpp(m-n,m-n))
-  allocate(op%fph(m-n,n))
-  allocate(op%fhh(n,n)) 
+  allocate(op%fpp(N-A,N-A))
+  allocate(op%fph(N-A,A))
+  allocate(op%fhh(A,A)) 
   
   q = 1  ! block index
   
@@ -161,8 +161,8 @@ subroutine allocate_blocks(jbas,op)
      npp = 0 
      nhh = 0 
      
-     do i = 1,m   !looping over sp states
-        do j = i,m
+     do i = 1,N   !looping over sp states
+        do j = i,N
  
            ! check if the two sp states can exist in this block 
            tz1 = jbas%itzp(i)
@@ -208,10 +208,13 @@ subroutine allocate_blocks(jbas,op)
      allocate(op%mat(q)%gam(4)%X(nph,nph)) !Vphph
      allocate(op%mat(q)%gam(2)%X(npp,nph)) !Vppph
      allocate(op%mat(q)%gam(6)%X(nph,nhh)) !Vphhh
+     do i = 1,6
+        op%mat(q)%gam(i)%X = 0.0
+     end do 
      
      nph = 0 ; npp = 0 ; nhh = 0
-     do i = 1,m   !looping over sp states
-        do j = i,m
+     do i = 1,N   !looping over sp states
+        do j = i,N
            
            ! check if the two sp states can exist in this block 
            tz1 = jbas%itzp(i)
@@ -233,17 +236,17 @@ subroutine allocate_blocks(jbas,op)
                  npp = npp + 1
                  op%mat(q)%qn(1)%Y(npp,1) = i
                  op%mat(q)%qn(1)%Y(npp,2) = j
-                 op%mat(q)%pnt(1)%Z(npp) = m*(i-1) + j !search for an integer 
+                 op%mat(q)%pnt(1)%Z(npp) = N*(i-1) + j !search for an integer 
               case (1)                                 ! instead of a pair of them  
                  nph = nph + 1
                  op%mat(q)%qn(2)%Y(nph,1) = i
                  op%mat(q)%qn(2)%Y(nph,2) = j 
-                 op%mat(q)%pnt(2)%Z(nph) = m*(i-1) + j 
+                 op%mat(q)%pnt(2)%Z(nph) = N*(i-1) + j 
               case (2) 
                  nhh = nhh + 1
                  op%mat(q)%qn(3)%Y(nhh,1) = i
                  op%mat(q)%qn(3)%Y(nhh,2) = j 
-                 op%mat(q)%pnt(3)%Z(nhh) = m*(i-1) + j 
+                 op%mat(q)%pnt(3)%Z(nhh) = N*(i-1) + j 
            end select
                      
         end do 
@@ -287,11 +290,11 @@ subroutine read_interaction(H,intfile,jbas)
      
      i1 = 1
      i2 = 1
-     int1 = H%msp*(a-1) + b
-     int2 = H%msp*(c-1) + d
+     int1 = H%Nsp*(a-1) + b
+     int2 = H%Nsp*(c-1) + d
      
      ! find the indeces
-     print*, c1,c2
+    
      do 
         if (int1 == H%mat(q)%pnt(C1)%Z(i1) ) exit
         i1 = i1 + 1
@@ -302,9 +305,7 @@ subroutine read_interaction(H,intfile,jbas)
         i2 = i2 + 1
      end do 
      
-     
-     print*, q,qx,i1,i2,J,Tz,Par,a,b,c,d
-     ! always make sure the input file has everything ordered so that a<=b, c<=d 
+    ! always make sure the input file has everything ordered so that a<=b, c<=d 
      If (C1>C2) then 
         H%mat(q)%gam(qx)%X(i2,i1)  = V 
      else
@@ -341,6 +342,151 @@ integer function block_index(J,T,P)
 end function 
 !=================================================================     
 !=================================================================
+real(8) function v_elem(a,b,c,d,J,T,P,op,jbas) 
+  ! grabs the matrix element you are looking for
+  implicit none
+  
+  integer :: a,b,c,d,J,T,P,q,qx,c1,c2
+  integer :: int1,int2,pre,i1,i2
+  type(sq_op) :: op 
+  type(spd) :: jbas
+ 
+  q = block_index(J,T,P) 
+     
+  C1 = jbas%con(a)+jbas%con(b) + 1 !ph nature
+  C2 = jbas%con(c)+jbas%con(d) + 1
+    
+  qx = C1*C2
+  qx = qx + adjust_index(qx)   !Vpppp nature  
+     
+  i1 = 1
+  i2 = 1
+  
+  pre = 1 
+  
+  ! get the indeces in the correct order
+  if ( a > b )  then 
+     int1 = op%Nsp*(b-1) + a 
+     pre = -1
+  else
+     int1 = op%Nsp*(a-1) + b
+  end if 
+  
+  if (c > d)  then     
+     int2 = op%Nsp*(d-1) + c
+     pre =  -1* pre
+  else 
+     int2 = op%Nsp*(c-1) + d
+  end if 
+     
+
+! find the indeces
+  do 
+     if (int1 == op%mat(q)%pnt(C1)%Z(i1) ) exit
+     i1 = i1 + 1
+  end do 
+        
+  do 
+     if (int2 == op%mat(q)%pnt(C2)%Z(i2) ) exit
+     i2 = i2 + 1
+  end do 
+
+  ! grab the matrix element
+   If (C1>C2) then 
+      v_elem = op%mat(q)%gam(qx)%X(i2,i1) * op%herm * pre 
+   else
+      v_elem = op%mat(q)%gam(qx)%X(i1,i2) * pre
+   end if 
+  
+end function 
+!=====================================================
+!=====================================================
+!=======================================================
+!=======================================================
+subroutine calculate_h0_harm_osc(hw,jbas,H,Htype) 
+  ! fills out the one body piece of the hamiltonian
+  implicit none 
+  
+  integer :: i,j,mass,Htype,c1,c2,cx
+  integer :: ni,li,ji,nj,lj,jj,tzi,tzj
+  real(8) :: hw,kij,T
+  type(sq_op) :: H 
+  type(spd) :: jbas
+
+  
+  !Htype =  |[1]: T - Tcm + V |[2]: T + Uho + V |[3]: T+V | 
+
+ 
+  mass = H%Abody
+  
+  do i = 1, H%Nsp
+     ni = jbas%nn(i) 
+     li = jbas%ll(i) 
+     ji = jbas%jj(i)
+     tzi = jbas%itzp(i) 
+     c1 = jbas%con(i) 
+     do j = i,H%Nsp
+        lj = jbas%ll(j) 
+        if (li .ne. lj) cycle 
+        jj = jbas%jj(j) 
+        if (ji .ne. jj) cycle
+        tzj = jbas%itzp(j) 
+        if (tzi .ne. tzj) cycle
+        c2 = jbas%con(j)
+        ! standard HO relations
+        if (ni == nj) then 
+           kij = 0.5*hw*(2*nj + lj +1.5)         
+        else if (ni == nj - 1) then 
+           kij = 0.5*hw * sqrt(nj*(nj+lj+0.5))            
+        else if (ni == nj + 1) then 
+           kij = 0.5*hw * sqrt((nj+1)*(nj+lj+1.5))
+        end if 
+        
+        select case (Htype) 
+           case(1) 
+              T =  kij*(1.d0-1.d0/mass) 
+           case(2) 
+              T = 2*kij*kron_del(ni,nj)
+           case(3)
+              T = kij 
+        end select 
+        
+        cx = c1+c2 
+        
+        select case (cx) 
+           case(0) 
+              H%fpp(i-mass,j-mass) = T
+              H%fpp(j-mass,i-mass) = T
+           case(1) 
+              if (c2 > c1) then 
+                 H%fph(i-mass,j) = T
+              else
+                 H%fph(j-mass,i) = T
+              end if 
+           case(2) 
+              H%fhh(i,j) = T
+              H%fhh(j,i) = T
+        end select
+     
+     end do
+  end do 
+
+end subroutine         
+!====================================================
+!====================================================  
+integer function kron_del(i,j) 
+  implicit none 
+  
+  integer :: i,j 
+  
+  if (i == j) then
+     kron_del = 1
+  else
+     kron_del = 0
+  end if 
+end function
+!====================================================
+!====================================================  
 end module
        
   
--- main_IMSRG.f90
+++ main_IMSRG.f90
@@ -6,17 +6,24 @@ program main_IMSRG
   type(spd) :: jbasis 
   type(sq_op) :: HS 
   character(50) :: sp_input_file,interaction_file
-  integer :: i
+  integer :: i,T,P,J,a,b,c,d
+  real(8) :: hw 
   
-  HS%nbody = 2
+  HS%Abody = 2
   HS%herm = 1  
+  hw = 28.0
   sp_input_file ='nl4.sps'
   interaction_file = 'vsrg.int' 
   
-  call read_sp_basis(jbasis,sp_input_file,HS%nbody) 
+  call read_sp_basis(jbasis,sp_input_file,HS%Abody) 
+
+  call allocate_blocks(jbasis,HS)
    
-  call allocate_blocks(jbasis,HS) 
+  call read_interaction(HS,interaction_file,jbasis)
+ 
+  call calculate_h0_harm_osc(hw,jbasis,HS,1) 
   
-  call read_interaction(HS,interaction_file,jbasis) 
-  
+  print*, v_elem(4,14,14,6,6,1,0,HS,jbasis) 
+ 
 end program
+
